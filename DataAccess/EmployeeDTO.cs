9195fe0ec29530faa606dd08da0f9771e23d9146
yasin <yasin@YASIN>
2025-12-26 22:24:31 +0300

    Employee eklendi

---
+++ DataAccess/EmployeeDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace DataAccess
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class EmployeeDTO
+    {
+        [Required(ErrorMessage = "Çalışan adı boş bırakılamaz.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Lütfen bir tarih seçiniz.")]
+        public DateTime HireDate { get; set; } = DateTime.Now;
+
+        public string ContactInfo { get; set; } = string.Empty;
+    }
+}
