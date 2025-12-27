53c45813ae4f41031bfe564100940234cdd852b5
yasin <yasin@YASIN>
2025-12-27 05:09:50 +0300

    CMPE SUNUM

---
+++ DataAccess/SupervisorDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccess
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SupervisorDTO
+    {
+        [Required(ErrorMessage = "Lütfen bir personel seçiniz.")]
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur.")]
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        [StringLength(255)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
